61dd592c883b51b50e3f231026fcc75977b5b3d9
Divyosmi Goswami <goswamidivyosmi@gmail.com>
2020-10-28 16:06:42 +0530

    Update and rename linear_model/Base.py to neuron.py

---
+++ neuron.py
@@ -0,0 +1,9 @@
+import numpy as np
+class neuron:
+  def __init__(self,val):
+    self.val=val
+  def val():
+    return self.val
+  def weight():
+    self.weight = np.random.randn()
+    return self.weight
