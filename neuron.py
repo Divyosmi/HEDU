a732f8b10cc5d01369a9aa348b6341a312d15f5d
Divyosmi Goswami <goswamidivyosmi@gmail.com>
2020-11-13 23:05:08 +0530

    Delete neuron.py

--- neuron.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-class neuron:
-  def __init__(self,val):
-    self.val=val
-  def val():
-    return self.val
-  def weight():
-    self.weight = np.random.randn(len(self.val))
-    return self.weight
